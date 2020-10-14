1d7d1c21924750d52e088ef7476e8d1e808a1e96
立石　純也 <dorasmapnogizaka46@tateishijunyanoMacBook-Air.local>
2020-10-14 20:15:58 +0900

    practice

---
+++ ruby-5.rb
@@ -0,0 +1,8 @@
+subjects = ["国語","算数","理科","社会"]
+puts subjects[2]
+
+president_tall = {"トランプ"=>188, "バイデン"=>183, "ブッシュ"=>180}
+puts president_tall["トランプ"]
+
+nogizakachan_tall = {:白石麻衣=>162, :松村沙友理=>164, :生田絵梨花=>160}
+puts nogizakachan_tall[:松村沙友理]
